99fff7d8294264d9a2b9e773d39c767144fa3816
Suresh <sureshmkgn@gmail.com>
2023-09-26 22:33:01 +0530

    Structure Change

---
+++ AM.Project/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using AM.Common.Logging;
+using AM.Common.Vault;
+using AM.Common.Mvc;
+using AM.Common.Metrics;
+
+namespace AM.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseLogging()
+                .UseVault()
+                .UseLockbox()
+                .UseAppMetrics();
+    }
+}
