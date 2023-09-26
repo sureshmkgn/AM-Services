ceae96d1bd35dafff4415b667e0b5ec6ed07f4bd
Suresh <sureshmkgn@gmail.com>
2023-09-26 15:50:55 +0530

    lauch configuration upadated

--- FA.Services.Identity/Controllers/HomeController.cs
+++ FA.Services.Identity/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FA.Services.Identity.Controllers
@@ -6,6 +7,7 @@ namespace FA.Services.Identity.Controllers
     public class HomeController : ControllerBase
     {
         [HttpGet]
+        [AllowAnonymous]
         public IActionResult Get() => Ok("FA Identity Service");
     }
 }
